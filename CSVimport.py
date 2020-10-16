a9280d5834d805ead39ebcff8068a6e52622cb4e
KatarinaSendecka <k.sendecka@gmail.com>
2020-10-16 11:46:30 +0200

    oprava nacitania csv - pridany spravny separator

--- CSVimport.py
+++ CSVimport.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-data = pd.read_csv("Video_Games_Sales_as_at_22_Dec_2016.csv", error_bad_lines=False)
+data = pd.read_csv("Video_Games_Sales_as_at_22_Dec_2016.csv", sep=';',error_bad_lines=False)
 
-data.head()
 print(data)
