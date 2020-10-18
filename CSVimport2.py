c0186452bea8339fbc1bf54495026ef1c2d46d75
KatarinaSendecka <k.sendecka@gmail.com>
2020-10-18 17:17:32 +0200

    pridaný delimiter ;

--- CSVimport2.py
+++ CSVimport2.py
@@ -1,7 +1,7 @@
 import csv
 
 subor = open('Video_Games_Sales_as_at_22_Dec_2016.csv', 'r', encoding="utf-8")
-reader = csv.reader(subor)
+reader = csv.reader(subor, delimiter=';')
 videoData = [row for row in reader]
 subor.close()
 
