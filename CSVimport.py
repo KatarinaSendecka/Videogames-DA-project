55ca5d394f7eb46f96564fef89bbd3d2b62554a2
KatarinaSendecka <k.sendecka@gmail.com>
2020-10-24 12:44:59 +0200

    opravené final

--- CSVimport.py
+++ CSVimport.py
@@ -3,10 +3,8 @@ import pandas as pd
 filecsv = open("Video_Games_Sales.csv",encoding='utf-8')
 filecsv2 = open("Video_Games_Sales_clean.csv","w",encoding='utf-8')
 
-for row in filecsv:
-    bad_row = filecsv.readline()
-   
-    semicolon_count = 0
+for bad_row in filecsv:
+    
     for i in range(0, len(bad_row)):
         if bad_row[i] == ";" and bad_row[i+1] ==" ":
             bad_row = bad_row[:i] + "," + bad_row[i+1:]
